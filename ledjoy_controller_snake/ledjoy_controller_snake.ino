c755d65797df94e870afc34656950a03c281ccce
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-04-25 15:56:39 +0200

    Corrected surname in beerware-license

--- ledjoy_controller_snake/ledjoy_controller_snake.ino
+++ ledjoy_controller_snake/ledjoy_controller_snake.ino
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * Simon Bergkvist & Erik Forsberg wrote this file. As long as you retain this notice you
+ * Simon Bergqvist & Erik Forsberg wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy us a beer in return Poul-Henning Kamp
  * ----------------------------------------------------------------------------
